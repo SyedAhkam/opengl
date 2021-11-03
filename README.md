0112235ee3fce99945f797d619798132c2d66ed5
SyedAhkam <smahkam57@gmail.com>
2021-11-04 01:21:29 +0530

    add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+This is me following the [learnopengl guide](https://learnopengl.com)
---
+++ include/main.h
@@ -0,0 +1,5 @@
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+void processInput(GLFWwindow* window);
+
+void doRendering(GLFWwindow* window);
